f33a495b5c3c3a11d67fe7544b345b871b926032
Rahul Dubey <rahuldubey.vjti@gmail.com>
2020-03-15 00:05:59 -0700

    Adding constraints on table, implemented etl script and loaded all the data

--- data_modeling_postgres/etl.py
+++ data_modeling_postgres/etl.py
@@ -7,37 +7,48 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.DataFrame([pd.read_json(filepath,  typ='series', convert_dates=False)])
 
     # insert song record
-    song_data = 
+    song_columns_to_select = ['song_id', 'title', 'artist_id', 'year', 'duration']
+    song_data = df[song_columns_to_select].values[0].tolist()
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_columns_to_select = ['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']
+
+    artist_data = df[artist_columns_to_select].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
+def get_time_data_from_df(df, timestamp_col):
+    datetime_col= 'start_time'
+    df[datetime_col] =  pd.to_datetime(df[timestamp_col], unit='ms')
+    time_units = ['hour', 'day', 'week', 'month', 'year', 'weekday']
+    time_data_series = [df[datetime_col]]
+    for time_unit in time_units:
+        time_data_series.append(getattr(df[datetime_col].dt,time_unit).rename(time_unit))
+    return pd.concat(time_data_series, axis=1)
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page']=='NextSong']
 
     # convert timestamp column to datetime
-    t = 
+    t = get_time_data_from_df(df, 'ts') 
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = t.values.tolist()
+    column_labels = t.columns.tolist()
+    time_df = pd.DataFrame(time_data, columns=column_labels)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName','lastName','gender', 'level']] 
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -56,7 +67,9 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        # construct a songplay_id from user_id and timestamp as this combination will be unique (assuming user can play from only one platform at a time
+        songplay_id = 'u'+str(row.userId)+'_t'+str(row.ts)
+        songplay_data = (songplay_id, row.start_time, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent) 
         cur.execute(songplay_table_insert, songplay_data)
 
 
