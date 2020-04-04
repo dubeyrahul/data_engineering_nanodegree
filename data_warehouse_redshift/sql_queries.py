dcfba87930bc6fe57852be2366cc56bce390c169
Rahul Dubey <rahuldubey.vjti@gmail.com>
2020-04-04 00:22:53 -0700

    staging queries done

--- data_warehouse_redshift/sql_queries.py
+++ data_warehouse_redshift/sql_queries.py
@@ -17,18 +17,42 @@ time_table_drop = "DROP TABLE IF EXISTS time_dim"
 
 # CREATE TABLES
 
-staging_events_table_create= (f"""
-COPY staging_events
-FROM {config.get('S3', 'LOG_DATA')}
-CREDENTIALS {config.get('IAM_ROLE', 'ARN')}
-JSON {config.get('S3','LOG_JSONPATH')}
+staging_events_table_create= ("""
+CREATE TABLE IF NOT EXISTS staging_events (
+    artist varchar,
+    auth varchar NOT NULL,
+    firstName varchar,
+    gender char(1),
+    itemInSession int NOT NULL,
+    lastName varchar,
+    length float,
+    level varchar NOT NULL,
+    location varchar,
+    method char(3) NOT NULL,
+    page varchar NOT NULL,
+    registration float,
+    sessionId int NOT NULL,
+    song varchar,
+    status SMALLINT NOT NULL,
+    ts integer NOT NULL,
+    userAgent varchar,
+    userId varchar
+)
 """)
 
 staging_songs_table_create = ("""
-COPY staging_songs
-FROM {config.get('S3', 'SONG_DATA')}
-CREDENTIALS {config.get('IAM_ROLE', 'ARN')}
-JSON 'auto'
+CREATE TABLE IF NOT EXISTS staging_songs (
+    artist_id varchar NOT NULL,
+    artist_latitude float,
+    artist_location varchar,
+    artist_longitude float,
+    artist_name varchar,
+    duration float NOT NULL,
+    num_songs int NOT NULL,
+    song_id varchar NOT NULL,
+    title varchar NOT NULL,
+    year int NOT NULL
+)
 """)
 
 songplay_table_create = ("""
@@ -48,11 +72,19 @@ time_table_create = ("""
 
 # STAGING TABLES
 
-staging_events_copy = ("""
-""").format()
+staging_events_copy = (f"""
+COPY staging_events
+FROM {config.get('S3', 'LOG_DATA')}
+CREDENTIALS {config.get('IAM_ROLE', 'ARN')}
+JSON {config.get('S3','LOG_JSONPATH')}
+""")
 
 staging_songs_copy = ("""
-""").format()
+COPY staging_songs
+FROM {config.get('S3', 'SONG_DATA')}
+CREDENTIALS {config.get('IAM_ROLE', 'ARN')}
+JSON 'auto'
+""")
 
 # FINAL TABLES
 
