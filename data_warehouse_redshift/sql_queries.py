2e6ac95a31ac4727f7c62f06922f8f545310a3a4
Rahul Dubey <rahuldubey.vjti@gmail.com>
2020-04-03 23:34:03 -0700

    staging queries done

--- data_warehouse_redshift/sql_queries.py
+++ data_warehouse_redshift/sql_queries.py
@@ -7,20 +7,28 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay_fact"
+user_table_drop = "DROP TABLE IF EXISTS user_dim"
+song_table_drop = "DROP TABLE IF EXISTS song_dim"
+artist_table_drop = "DROP TABLE IF EXISTS artist_dim"
+time_table_drop = "DROP TABLE IF EXISTS time_dim"
 
 # CREATE TABLES
 
-staging_events_table_create= ("""
+staging_events_table_create= (f"""
+COPY staging_events
+FROM {config.get('S3', 'LOG_DATA')}
+CREDENTIALS {config.get('IAM_ROLE', 'ARN')}
+JSON {config.get('S3','LOG_JSONPATH')}
 """)
 
 staging_songs_table_create = ("""
+COPY staging_songs
+FROM {config.get('S3', 'SONG_DATA')}
+CREDENTIALS {config.get('IAM_ROLE', 'ARN')}
+JSON 'auto'
 """)
 
 songplay_table_create = ("""
