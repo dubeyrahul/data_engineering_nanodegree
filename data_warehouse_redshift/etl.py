8725de8f703fee227e13e54d8207aa5440fc4fe1
Rahul Dubey <rahuldubey.vjti@gmail.com>
2020-04-05 14:06:24 -0700

    adding diagram to README

--- data_warehouse_redshift/etl.py
+++ data_warehouse_redshift/etl.py
@@ -4,6 +4,13 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 separator = "-"*30
 def load_staging_tables(cur, conn):
+    """
+    Loads data from S3 to the staging tables
+    :param postgres-cursor cur: Postgres DB cursor
+    :param postgres-connection conn: Postgres DB connection
+    :return: None
+    :rtype: None
+    """
     print("Load staging tables from S3")
     for query in copy_table_queries:
         print("Running:\n"+query)
@@ -14,6 +21,13 @@ def load_staging_tables(cur, conn):
 
 
 def insert_tables(cur, conn):
+    """
+    Loads and transforms data from staging tables to dimensional model
+    :param postgres-cursor cur: Postgres DB cursor
+    :param postgres-connection conn: Postgres DB connection
+    :return: None
+    :rtype: None
+    """
     print("Inserting into tables from staging tables")
     for query in insert_table_queries:
         print("Running:\n"+query)
@@ -24,13 +38,15 @@ def insert_tables(cur, conn):
 
 
 def main():
+    # Loading the dwh.cfg file to load Redshift cluster config
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
-
+    # Setup connection to the redshift db
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
-
+    # Load data from S3 to staging_tables
     load_staging_tables(cur, conn)
+    # Load and transform data from staging_tables to dimesional model
     insert_tables(cur, conn)
 
     conn.close()
